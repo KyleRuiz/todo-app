4f976137787b82d7623c90c75f4a8a42a679a29c
Kyle <kyleruiz1515@gmail.com>
2023-04-25 21:06:56 +0900

    컴포넌트 성능 최적화

--- src/App.js
+++ src/App.js
@@ -2,28 +2,39 @@ import './App.css';
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
-import {useState, useRef, useCallback} from "react";
+import {useReducer, useState, useRef, useCallback} from "react";
+
+const createBulkTodos = () => {
+  const array =[];
+  for(let i = 0; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+const todoReducer = (todos, action) => {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map((todo) => todo.id === action.id ? {...todo, checked: !todo.checked} : todo,);
+    default:
+      return todos;
+  }
+}
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id:1,
-      text: '리액트의 기초 알아보기',
-      checked: true
-    },
-    {
-      id:2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true
-    },
-    {
-      id:3,
-      text: '일정 관리 앱 만들어 보기',
-      checked: false
-    }
-  ]);
 
-  const nextId = useRef(4);
+
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+
+  const nextId = useRef(2501);
 
   const onInsert = useCallback((text) => {
     const todo = {
@@ -31,17 +42,17 @@ function App() {
       text,
       checked: false
     };
-    setTodos(todos.concat(todo));
+    dispatch({ type: 'INSERT', todo});
     nextId.current += 1;
-  }, [todos],);
+  }, []);
 
   const onRemove = useCallback((id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  }, [todos]);
+    dispatch({type: 'REMOVE', id});
+  }, []);
 
   const onToggle = useCallback((id) => {
-    setTodos(todos.map((todo) => todo.id === id ? {...todo, checked: !todo.checked} : todo,),);
-  }, [todos],);
+    dispatch({type: 'TOGGLE', id});
+  }, []);
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert}/>
